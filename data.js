3213abdc56ec21a2114c2c34f81a971f8c71a64d
mesut <gelukkig.zijn.be@gmail.com>
2020-03-28 12:19:37 +0100

    data.js v4

--- data.js
+++ data.js
@@ -1,35 +1,36 @@
 
 var todoList = {
     // it should store the todos array on an object.
-    todos: ['item 1', 'item 2', 'item 3'],
+    todos: [],
     // it should have a function to display todos.
     displayTodos: function () { // now it (and the others) is a method
         console.log('My todos: ', this.todos);
     },
     // it should have an addTodo method. 
     addTodo: function (todo) {
-        this.todos.push(todo);
+        this.todos.push({
+            todoText: todo,
+            completed: false
+        });
         this.displayTodos();
     },
     // it should have a changeTodo method.
     changeTodo: function (position, newValue) {
-        this.todos[position] = newValue;
+        this.todos[position].todoText = newValue;
         this.displayTodos();
     },
     // It should have a deleteTodo method.
     deleteTodo: function (position) {
         this.todos.splice(position, 1); // 5. Delete method
         this.displayTodos();
+    },
+    toggleCompleted: function (position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();    
     }
 };
 
 todoList.displayTodos();
-//  add new todos.
-todoList.addTodo('item 4');
-todoList.addTodo('item 5');
-
-// change a todo.
-todoList.changeTodo(0, 'item 1 updated');
-
-// delete a todo.
-todoList.deleteTodo(1);
+todoList.addTodo('boolean testing');
+todoList.toggleCompleted(0);
